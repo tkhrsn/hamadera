7acca8ded9b167d80e821a842ce2d8f9b200d1e8
unknown <>
2017-03-02 21:41:16 +0900

    noriaki

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,16 +5,14 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>はまでら</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
         <!-- Styles -->
         <style>
             html, body {
                 background-color: #fff;
-                color: #636b6f;
+                //color: #636b6f;
                 font-family: 'Raleway', sans-serif;
                 font-weight: 100;
                 height: 100vh;
@@ -45,10 +43,6 @@
                 text-align: center;
             }
 
-            .title {
-                font-size: 84px;
-            }
-
             .links > a {
                 color: #636b6f;
                 padding: 0 25px;
@@ -64,7 +58,9 @@
             }
         </style>
     </head>
-    <body>
+    <body background="img/noriaki.jpg">
+      <font size="50">This is Hamadera Style.</font>
+      <div align="center"><font size="6"><b><font color="#c20000">どこ</font><font color="#cc5c00">相手</font><font color="#d6c100">にし</font><font color="#92e100">ても</font><font color="#2feb00">リア</font><font color="#00f53d">ルに</font><font color="#00ffb3">言う</font><font color="#00e1f6">。そ</font><font color="#007eee">れが</font><font color="#0021e5">大同</font><font color="#3500dc">生命</font><font color="#8500d3">辞め</font><font color="#cb00c8">る理</font><font color="#c20074">由</font></b></font></div>
       <marquee>服を選ぶなあ</marquee>
       <marquee width="90%">チョンテセやんけ！テポドン落とさんといてな！</marquee>
       <marquee behavior="scroll">高い視点に立ってより本質的な仕事ができる</marquee>
@@ -73,7 +69,6 @@
       <marquee direction="left">風邪ですか？早くやって、早く寝ましょう！</marquee>
       <marquee direction="right">例によって、今日も台本作ってきました。</marquee>
       <marquee scrolldelay="100">陛陛陛！！幼幼幼！！</marquee>
-      <marquee scrollamount="8">まじで小池さんそっくり行きたいな、と思った。</marquee>
-      <div align="center"><font size="6"><b><font color="#c20000">どこ</font><font color="#cc5c00">相手</font><font color="#d6c100">にし</font><font color="#92e100">ても</font><font color="#2feb00">リア</font><font color="#00f53d">ルに</font><font color="#00ffb3">言う</font><font color="#00e1f6">。そ</font><font color="#007eee">れが</font><font color="#0021e5">大同</font><font color="#3500dc">生命</font><font color="#8500d3">辞め</font><font color="#cb00c8">る理</font><font color="#c20074">由</font></b></font></div>
+      <marquee scrollamount="8">まじで小池さんそっくりやん行きたいな、と思った。</marquee>
     </body>
 </html>
